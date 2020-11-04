283ad07116fc12b0d872668adf031b95ce86963d
Nechrito <philip.lindh98@hotmail.com>
2020-11-04 17:31:53 +0100

    init

--- projects/RealTimeEngine/code/RealTimeEngine.cpp
+++ projects/RealTimeEngine/code/RealTimeEngine.cpp
@@ -22,55 +22,7 @@ bool isInteger(const string& s)
    return (*p == 0);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char** argv) 
 {
-	if(argc == 1) 
-    {
-		printf("No arguments, running lightning program \n");
-
-		Application app;
-		if (app.Open())
-		{
-			app.Run();
-		}
-	}
-    else 
-    {
-		HardwareSpecs specs;
-
-		for(int i = 1; i < argc; i++)
-		{
-			auto argument = string(argv[i]);
-
-			if (isInteger(argument))
-			{
-				continue;
-			}
-
-			// Print num processors, hostname, hardware platform & total amount of memory
-			if (argument == "-i")
-			{
-				auto processors = specs.GetProcessors();
-				printf("Processors: %d \n", processors);
-			}
-
-			// Fork an X amount of processes that each print out its PID, calculate the sum
-			else if (argument == "-f")
-			{
-				int forkCount = atoi(argv[i + 1]);
-    			cout << "Calculating sum on " << forkCount << " forks" << endl;
-				specs.ForkSum(forkCount);
-			}
-
-			// Similar to -f but using threads instead of forks
-			else if (argument == "-t")
-			{
-				int threadCount = atoi(argv[i + 1]);
-    			cout << "Calculating sum on " << threadCount << " threads" << endl;
-				specs.ThreadSum(threadCount);
-			}
-		} 
-    }
-
 	
 }
